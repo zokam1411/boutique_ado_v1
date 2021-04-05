4be5b827283c27a9e3b71b7a1df47c6496c5f267
zokam1411 <zokam1411@gmail.com>
2021-04-05 23:31:10 +0000

    Add bag app, urls and template

---
+++ bag/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def view_bag(request):
+    """ A view that renders a bag content page """
+
+    return render(request, 'bag/bag.html')
